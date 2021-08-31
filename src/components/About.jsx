e901cd7bdf93dca67deb1a04c4341e74c304c719
clecardona <clecardona@gmail.com>
2021-08-31 16:29:58 +0200

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,15 +3,18 @@ import React from "react";
 export default function About() {
   return (
     <section id="about" className="section-about">
-      <img alt="portrait" src="http://clecardona.com/img/portrait.jpg" />
-      <p>
-        <h2>About me</h2>
-        Hi! I am Clement, a junior frontend developer from Nice, France.This is
-        my Portfolio, which I use to showcase everything I develop. I specialize
-        in React.js development with TypeScript with a intuitive design and
-        attention to details. Why do I need intuitive design and attention to
-        details , you might ask? Well, scroll down and let me convince you.
-      </p>
+      <div className="wrapper">
+        <img alt="portrait" src="http://clecardona.com/img/portrait.jpg" />
+        <p>
+          <h2>About me</h2>
+          Hi! I am Clement, a junior frontend developer from Nice, France.This
+          is my Portfolio, which I use to showcase everything I develop. I
+          specialize in React.js development with TypeScript with a intuitive
+          design and attention to details. Why do I need intuitive design and
+          attention to details , you might ask? Well, scroll down and let me
+          convince you.
+        </p>
+      </div>
     </section>
   );
 }
