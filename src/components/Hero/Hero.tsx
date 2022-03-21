e57118cb6074ca6911e65c299c3b5f6556dd2bde
clecardona <clecardona@gmail.com>
2022-03-21 15:40:11 +0100

    import img from other place

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,9 +1,6 @@
 import Bubble from "./Bubble";
 
-
 const Hero = (): JSX.Element => {
-
-
   return (
     <section id="hero" className="section-hero">
       <img
@@ -15,8 +12,8 @@ const Hero = (): JSX.Element => {
         <Bubble />
         <img
           className="illustration"
-          alt="illustrattion"
-          src="https://firebasestorage.googleapis.com/v0/b/npa-portfolio.appspot.com/o/illustration-portfolio.png?alt=media&token=e27b56d0-ab40-469b-af3e-19e7a07a9c05"
+          alt="illustration"
+          src="https://clecardona.com/npa/portfolio/img/illustration-portfolio.png"
         />
       </div>
     </section>
