086960fe87dd028d6624bc6599ecf842890fb5bf
clecardona <clecardona@gmail.com>
2022-09-16 11:28:21 +0200

    wip translation

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -4,26 +4,22 @@ import { useTheme } from 'state/ThemeProvider';
 import CV from './CV/CV';
 
 const AboutMe = (): JSX.Element => {
-  const { isCVOpen, setCVOpen } = useTheme()
-  const { t } = useTranslation()
-  return (
-    <>
-      <div className='about-content'>
-        <img
-          className='about-img'
-          alt='portrait'
-          src='http://clecardona.com/img/portrait.jpg'
-        />
-        <p className='about-paragraph'>{t("accordion.about.paragraph")}</p>
-      </div>
-      <div className='additional-content'>
-        <button className='btn btn-more' onClick={() => setCVOpen(!isCVOpen)}>
-          <h3>{isCVOpen ? "-" : "+"}</h3>
-          <h3>{isCVOpen ? "Less" : "More"}</h3>
-        </button>
-        <CV />
-      </div>
-    </>
-  )
+    const { isCVOpen, setCVOpen } = useTheme()
+    const { t } = useTranslation()
+    return (
+        <>
+            <div className="about-content">
+                <img className="about-img" alt="portrait" src="http://clecardona.com/img/portrait.jpg" />
+                <p className="about-paragraph">{t('accordion.about.paragraph')}</p>
+            </div>
+            <div className="additional-content">
+                <button className="btn btn-more" onClick={() => setCVOpen(!isCVOpen)}>
+                    <h3>{isCVOpen ? '-' : '+'}</h3>
+                    <h3>{isCVOpen ? t('accordion.about.less') : t('accordion.about.more')}</h3>
+                </button>
+                <CV />
+            </div>
+        </>
+    )
 }
 export default AboutMe
