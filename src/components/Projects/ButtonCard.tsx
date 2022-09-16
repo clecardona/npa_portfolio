b57878d36ae7b9623c35bde2f737623724dc5b08
clecardona <clecardona@gmail.com>
2022-09-16 13:21:02 +0200

    feat: animate in modal

--- src/components/Projects/ButtonCard.tsx
+++ src/components/Projects/ButtonCard.tsx
@@ -1,55 +1,49 @@
 // npm packages
-import { useState } from "react";
-import { CSSTransition } from "react-transition-group";
+import { useState } from 'react';
+
 // Local imports
-import Modal from "./Modal";
+import Modal from './Modal';
 
 interface IItem {
-  item: {
-    title: string;
-    isReleased: boolean;
-    thumbnailURL: string;
-    thumbnailTitle: string;
-  };
+    item: {
+        title: string
+        isReleased: boolean
+        thumbnailURL: string
+        thumbnailTitle: string
+    }
 }
 
 const ButtonCard = ({ item }: IItem): JSX.Element => {
-  const [isOpen, setIsOpen] = useState(false);
-  const { title, isReleased, thumbnailURL, thumbnailTitle } = item;
+    const [isOpen, setIsOpen] = useState(false)
+    const { title, isReleased, thumbnailURL, thumbnailTitle } = item
 
-  // Components
-  const NotReleased = () => (
-    <div className="card-overlay">
-      <p className="comingsoon">COMING</p>
-      <p className="comingsoon">SOON ...</p>
-    </div>
-  );
-
-  return (
-    <li>
-      <button
-        className="card"
-        disabled={!item.isReleased}
-        onClick={() => setIsOpen(true)}
-      >
-        <div className="card-inner">
-          <div className="card-front">
-            <div className="card-img">
-              {!isReleased && <NotReleased />}
-              <img alt={title} src={thumbnailURL} />
-            </div>
-          </div>
-
-          <div className="card-back">
-            <h3>{thumbnailTitle}</h3>
-          </div>
+    // Components
+    const NotReleased = () => (
+        <div className="card-overlay">
+            <p className="comingsoon">COMING</p>
+            <p className="comingsoon">SOON ...</p>
         </div>
-      </button>
+    )
 
-      <CSSTransition in={isOpen} timeout={500} classNames="modal-overlay">
-        <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} item={item} />
-      </CSSTransition>
-    </li>
-  );
-};
-export default ButtonCard;
+    return (
+        <li>
+            <button className="card" disabled={!item.isReleased} onClick={() => setIsOpen(true)}>
+                <div className="card-inner">
+                    <div className="card-front">
+                        <div className="card-img">
+                            {!isReleased && <NotReleased />}
+                            <img alt={title} src={thumbnailURL} />
+                        </div>
+                    </div>
+
+                    <div className="card-back">
+                        <h3>{thumbnailTitle}</h3>
+                    </div>
+                </div>
+            </button>
+
+            <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} item={item} />
+        </li>
+    )
+}
+export default ButtonCard
--- src/components/Projects/Modal.tsx
+++ src/components/Projects/Modal.tsx
@@ -1,120 +1,113 @@
-//npm packages
-import reactDom from "react-dom";
-import { CSSTransition } from "react-transition-group";
+import reactDom from 'react-dom';
+
+import github from 'assets/icns/github.png';
+import link from 'assets/icns/link.png';
+import icons from 'assets/icons.json';
 import {
-  IoIosTabletLandscape,
-  IoIosLaptop,
   IoIosDesktop,
+  IoIosLaptop,
   IoIosPhonePortrait,
-} from "react-icons/io";
+  IoIosTabletLandscape,
+} from 'react-icons/io';
 
-//Local imports
-import link from "assets/icns/link.png";
-import github from "assets/icns/github.png";
-
-import icons from "assets/icons.json";
-import Pills from "./Pills";
+import Pills from './Pills';
 
 interface IModalProps {
-  isOpen: boolean;
-  onClose: () => void;
-  item: any;
+    isOpen: boolean
+    onClose: () => void
+    item: any
 }
 
 interface IButtonLink {
-  design: string;
-  label: string;
-  link: string;
-  icon?: string;
+    design: string
+    label: string
+    link: string
+    icon?: string
 }
 
 const Modal = ({ isOpen, onClose, item }: IModalProps) => {
-  const technologies8 = item.technologies.slice(0, 8);
+    const technologies8 = item.technologies.slice(0, 8)
 
-  // Components
-  const ButtonLink = ({ design, icon, label, link }: IButtonLink) => (
-    <a className={design} target="blank" href={link}>
-      {icon && <img src={icon} alt="" />}
-      <h3>{label}</h3>
-    </a>
-  );
+    const ButtonLink = ({ design, icon, label, link }: IButtonLink) => (
+        <a className={design} target="blank" href={link}>
+            {icon && <img src={icon} alt="" />}
+            <h3>{label}</h3>
+        </a>
+    )
 
-  if (!isOpen) return null;
+    if (!isOpen) return <></>
+    return reactDom.createPortal(
+        <>
+            <div className={isOpen ? 'modal-overlay' : 'modal-overlay out'} onClick={onClose} />
+            <div className="modal-wrapper">
+                <div className="modal">
+                    <button className="btn-close" onClick={onClose}>
+                        <img alt="close" src={icons.cross} />
+                    </button>
 
-  return reactDom.createPortal(
-    <>
-      <div className="modal-overlay" onClick={onClose} />
-      <div className="modal-wrapper">
-        <CSSTransition in={isOpen} timeout={500} classNames="modal">
-          <div className="modal">
-            <button className="btn-close" onClick={onClose}>
-              <img alt="close" src={icons.cross} />
-            </button>
+                    <div className="modal-img">
+                        <img alt={item.name} src={item.screenshotURL} />
+                    </div>
 
-            <div className="modal-img">
-              <img alt={item.name} src={item.screenshotURL} />
-            </div>
+                    <h1>{item.title}</h1>
+                    <p>{item.description}</p>
 
-            <h1>{item.title}</h1>
-            <p>{item.description}</p>
+                    <div className="pills-container">
+                        <Pills technologies={technologies8} />
+                    </div>
 
-            <div className="pills-container">
-              <Pills technologies={technologies8} />
-            </div>
+                    <div className="mq-container" style={{ height: 30 }}>
+                        {item.responsive && item.responsive.mobile && (
+                            <div className="mq-item">
+                                <p>Mobile</p>
+                                <IoIosPhonePortrait size={22} style={{ marginTop: 8 }} />
+                            </div>
+                        )}
+                        {item.responsive && item.responsive.tablet && (
+                            <div className="mq-item">
+                                <p>Tablet</p>
+                                <IoIosTabletLandscape size={24} style={{ marginTop: 9 }} />
+                            </div>
+                        )}
+                        {item.responsive && item.responsive.laptop && (
+                            <div className="mq-item">
+                                <p>Laptop</p>
+                                <IoIosLaptop size={30} style={{ marginTop: 5 }} />
+                            </div>
+                        )}
 
-            <div className="mq-container" style={{ height: 30 }}>
-              {item.responsive && item.responsive.mobile && (
-                <div className="mq-item">
-                  <p>Mobile</p>
-                  <IoIosPhonePortrait size={22} style={{ marginTop: 8 }} />
+                        {item.responsive && item.responsive.desktop && (
+                            <div className="mq-item">
+                                <p>Desktop</p>
+                                <IoIosDesktop size={30} style={{ marginTop: 3 }} />
+                            </div>
+                        )}
+                    </div>
+
+                    <div className="buttons">
+                        {item.websiteURL && (
+                            <ButtonLink
+                                design="btn btn-main"
+                                link={item.websiteURL}
+                                icon={link}
+                                label="Visit Website"
+                            />
+                        )}
+
+                        {item.githubURL && (
+                            <ButtonLink
+                                design="btn btn-ghost"
+                                link={item.githubURL}
+                                icon={github}
+                                label="Git repository"
+                            />
+                        )}
+                    </div>
                 </div>
-              )}
-              {item.responsive && item.responsive.tablet && (
-                <div className="mq-item">
-                  <p>Tablet</p>
-                  <IoIosTabletLandscape size={24} style={{ marginTop: 9 }} />
-                </div>
-              )}
-              {item.responsive && item.responsive.laptop && (
-                <div className="mq-item">
-                  <p>Laptop</p>
-                  <IoIosLaptop size={30} style={{ marginTop: 5 }} />
-                </div>
-              )}
-
-              {item.responsive && item.responsive.desktop && (
-                <div className="mq-item">
-                  <p>Desktop</p>
-                  <IoIosDesktop size={30} style={{ marginTop: 3 }} />
-                </div>
-              )}
             </div>
-
-            <div className="buttons">
-              {item.websiteURL && (
-                <ButtonLink
-                  design="btn btn-main"
-                  link={item.websiteURL}
-                  icon={link}
-                  label="Visit Website"
-                />
-              )}
-
-              {item.githubURL && (
-                <ButtonLink
-                  design="btn btn-ghost"
-                  link={item.githubURL}
-                  icon={github}
-                  label="Git repository"
-                />
-              )}
-            </div>
-          </div>
-        </CSSTransition>
-      </div>
-    </>,
-    // @ts-ignore
-    document.getElementById("portal")
-  );
-};
-export default Modal;
+        </>,
+        // @ts-ignore
+        document.getElementById('portal'),
+    )
+}
+export default Modal
